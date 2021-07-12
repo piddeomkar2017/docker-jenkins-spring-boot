492b3e7e8ef01e97f51da9e3932117a07ea372a2
piddeomkar2017 <piddeomkar2017@gmail.com>
2021-07-12 12:25:41 +0530

    second commit

---
+++ src/main/java/com/omkar/microservice/DockerJenkinsSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.omkar.microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerJenkinsSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerJenkinsSpringBootApplication.class, args);
+	}
+
+}
