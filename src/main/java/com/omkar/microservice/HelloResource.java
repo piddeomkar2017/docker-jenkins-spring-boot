ae8948a43a1b749ad69684495e0565e4f57f3f66
piddeomkar2017 <74731155+piddeomkar2017@users.noreply.github.com>
2021-08-10 12:47:53 +0530

    changed function name

--- src/main/java/com/omkar/microservice/HelloResource.java
+++ src/main/java/com/omkar/microservice/HelloResource.java
@@ -14,7 +14,7 @@ public class HelloResource {
 		return "Hello World from Omkar. I am learning devops....just checking...I am Omkar Pidde...again doing internal review...Internal review completed....todays date is 10/08/2021";
 	}
 	@GetMapping("/hi")
-	public String hello()
+	public String hello1()
 	{
 		return "hi from Omkar Pidde....todays date is 10/08/2021";
 	}
